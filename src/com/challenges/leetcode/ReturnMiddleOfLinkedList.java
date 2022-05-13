01fc67982e2bad23af5ef5af7ee04e71c020f58e
Chetan-kond <49838554+chetankondawle@users.noreply.github.com>
2022-05-14 00:31:51 +0530

    Create ReturnMiddleOfLinkedList.java

---
+++ src/com/challenges/leetcode/ReturnMiddleOfLinkedList.java
@@ -0,0 +1,31 @@
+package com.challenges.leetcode;
+
+public class ReturnMiddleOfLinkedList {
+	/**
+	 * Definition for singly-linked list.
+	 * public class ListNode {
+	 *     int val;
+	 *     ListNode next;
+	 *     ListNode() {}
+	 *     ListNode(int val) { this.val = val; }
+	 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	 * }
+	 */
+	
+	public ListNode middleNode(ListNode head) {
+        ListNode middle = head;
+        ListNode tail = head;
+        while(tail !=null && tail.next != null){
+            middle = middle.next;
+            tail = tail.next.next;
+        }
+        
+        return middle;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
